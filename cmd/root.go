595e78e207f78fc5525963e41cee57c1b95e83c8
Marc Brandenburg <git@marcbrandenburg.net>
2025-06-07 11:15:03 +0200

    chore: add fsnotify dependency

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,43 @@ var rootCmd = &cobra.Command{
 	Short: "Test application for GitHub Actions",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello World!")
+
+		// Create new watcher.
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer watcher.Close()
+
+		// Start listening for events.
+		go func() {
+			for {
+				select {
+				case event, ok := <-watcher.Events:
+					if !ok {
+						return
+					}
+					log.Println("event:", event)
+					if event.Has(fsnotify.Write) {
+						log.Println("modified file:", event.Name)
+					}
+				case err, ok := <-watcher.Errors:
+					if !ok {
+						return
+					}
+					log.Println("error:", err)
+				}
+			}
+		}()
+
+		// Add a path.
+		err = watcher.Add("/tmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Block main goroutine forever.
+		<-make(chan struct{})
 	},
 }
 
